ede4ab3e8cff4102d77d9ff73b5d2134832aca1a
marslanali <arslanali800@gmail.com>
2019-12-14 21:25:09 +0500

    adding new files via git

--- zmq_matlab_examples/publisher.m
+++ zmq_matlab_examples/publisher.m
@@ -4,19 +4,18 @@ function pub_server(varargin)
     % separated by a space, the first one is the CEP (ZIP brazilian equivalent)
     % and the second is the temperature in celsius
 
-    port = 5556;
+    port = 5552;
 
     context = zmq.core.ctx_new();
-    socket = zmq.core.socket(context, 'ZMQ_PUB');
-    zmq.core.bind(socket, sprintf('tcp://*:%d', port));
+    publisher = zmq.core.socket(context, 'ZMQ_PUB');
+    zmq.core.bind(publisher, sprintf('tcp://*:%d', port));
 
-    fprintf('Broadcasting temperature information...\n');
+    fprintf('Broadcasting information...\n');
     while (1)
-        topic = randi([15198, 15209], 1, 1); % Choose a brazilian CEP code (first 5 digits)
-        data = randi([10, 45]);              % Pick a random temperature
-        message = sprintf('%d %d', topic, data);
-        fprintf('%s\n', message);
-        zmq.core.send(socket, uint8(message));
+        
+        message = sprintf('%d %0.4f ', 10001, 3.21);
+        fprintf('Sending %s\n', message);
+        zmq.core.send(publisher, uint8(message));
         delay(1);
     end
 end
---
+++ zmq_ubuntu_examples/zmq_subscriber/build/CMakeFiles/zmq_subscriber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zmq_subscriber.dir/main.cpp.o"
+  "zmq_subscriber"
+  "zmq_subscriber.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zmq_subscriber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
