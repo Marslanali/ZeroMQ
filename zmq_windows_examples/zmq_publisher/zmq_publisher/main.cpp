281e1844ceb972db7e3869e062157845a1989506
marslanali <arslanali800@hotmail.com>
2020-05-25 00:12:49 +0500

    refactoring with clang-format

--- zmq_windows_examples/zmq_publisher/zmq_publisher/main.cpp
+++ zmq_windows_examples/zmq_publisher/zmq_publisher/main.cpp
@@ -1,28 +1,23 @@
-#include <zmq.h>
-#include <stdio.h>
-#include <string.h>
 #include <assert.h>
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <zmq.h>
 
-int main(void)
-{
-	
+int main(void) {
 
-void * context = zmq_ctx_new();
-void * publisher = zmq_socket(context, ZMQ_PUB);
-zmq_bind(publisher, "tcp://*:5555");
-char buffer[30] = {};
+  void *context = zmq_ctx_new();
+  void *publisher = zmq_socket(context, ZMQ_PUB);
+  zmq_bind(publisher, "tcp://*:5555");
+  char buffer[30] = {};
 
-	while (1)
-	{
-		sprintf_s(buffer, "%d %0.4f ", 10001, 3.21);
-		zmq_send(publisher, buffer, 30, 0);
-		std::cout << "Pubishing...\n" <<buffer<< std::endl;
-	}
-	zmq_close(publisher);
-	zmq_ctx_destroy(context);
+  while (1) {
+    sprintf_s(buffer, "%d %0.4f ", 10001, 3.21);
+    zmq_send(publisher, buffer, 30, 0);
+    std::cout << "Pubishing...\n" << buffer << std::endl;
+  }
+  zmq_close(publisher);
+  zmq_ctx_destroy(context);
 
-	return 0;
-
-
+  return 0;
 }
